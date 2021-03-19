f59403a07b34103b02bd847749319ab90a971002
Jack-Moon628 <bright_96_628@outlook.com>
2021-03-18 22:15:37 -0500

    meta data

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,8 +4,6 @@ import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import "../style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import logo from "../Assets/logo.png";
-import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 
 function NavBar() {
